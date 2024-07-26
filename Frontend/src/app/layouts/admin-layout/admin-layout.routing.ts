104142fc73f8098729d9631f51c05055279565fe
ziedloukil987 <zied.loukil@esprit.tn>
2024-07-26 18:37:48 +0200

    basic user interface

--- Frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ Frontend/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -19,5 +19,5 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'maps',           component: MapsComponent },
     { path: 'notifications',  component: NotificationsComponent },
     { path: 'upgrade',        component: UpgradeComponent },
-    {path:'users' , component:UserTableComponent}
+    {path:'' , component:UserTableComponent}
 ];
